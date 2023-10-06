3a03b36f3b7251ec05d8189937ffe60df66b7e13
Sebas723 <147189749+Sebas723@users.noreply.github.com>
2023-10-06 17:08:20 -0500

    Add files via upload

--- Crud sencillo/js/js.js
+++ Crud sencillo/js/js.js
@@ -4,6 +4,8 @@ function app(){
     let nombre = new CRUD("ejemplo");
     nombre.create(["juan","carlos","alberto"]);
     nombre.create({ message : "hola mundo"});
+    nombre.update(1, "buenas tardes");
+    nombre.delete(2);
 
     let crud2 = new CRUD("ejemplo");
     console.log(nombre.readAll());
